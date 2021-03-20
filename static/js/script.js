35b8a3897d089bbe5f28c9e12a1b38f6e7cd5ecc
Sergey <derevankosergej0@gmail.com>
2021-03-20 22:20:35 +0200

    Deploy

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,26 @@ function openNav(id) {
 function closeNav(id) {
     document.getElementById(id).style.width = "0";
 }
+
+$('.slider_reviews').slick({
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	prevArrow:'<p class="prev but"><img src="static/images/chevron-left.png"></p>',
+	nextArrow:'<p class="next but"><img src="static/images/chevron-right.png"></p>',
+	responsive: [
+	{
+	  breakpoint: 1200,
+	  settings: {
+	    slidesToShow: 1
+	  }
+	},
+	{
+      breakpoint: 720,
+      settings: {
+        dots:true,
+        slidesToShow: 1
+      }
+    }
+	]
+});
